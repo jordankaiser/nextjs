eff8f94ba16ec1036086e06d950bf50a4452fc56
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2025-02-04 07:44:19 -0600

    add todo app

--- src/app/ui/todo.js
+++ src/app/ui/todo.js
@@ -11,17 +11,13 @@ export default function todo() {
         <ul>
           {todos.map((todo, index) => (
             <li key={index}>
-              <label htmlFor={`item-${index}`}>{todo}</label>
               <input
                 id={`item-${index}`}
                 type="checkbox"
-                onChange={(e) => {
-                  // When the checkbox is checked remove the corresponding todo
-                  if (e.target.checked) {
-                    setTodos((prevTodos) => prevTodos.filter((_, i) => i !== index));
-                  }
-                }}
               />
+              <label htmlFor={`item-${index}`}>{todo}</label>
+              <button>Edit</button>
+              <button onClick={() => handleDelete(index)}>Delete</button>
             </li>
           ))}
         </ul>
@@ -29,11 +25,19 @@ export default function todo() {
     }
   }
 
+  const handleDelete = (index) => {
+    const newTodos = todos.filter((todo, i) => i !== index);
+    setTodos(newTodos);
+  }
+
+  const handleAddTask = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  }
+
   const AddTodo = () => {
     const handleSubmit = (event) => {
       event.preventDefault();
-      const newTodo = event.target[0].value;
-      setTodos([...todos, newTodo]);
+      handleAddTask(event.target[0].value);
     }
 
     return (

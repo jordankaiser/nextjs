7c25c7b435c48bfa8a6ccda3fa6d0db8003fe59f
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2025-02-08 07:16:40 -0600

    update API call in fetchAIResponse to use new chat completion model and adjust request structure

--- src/app/utilities/apiService.js
+++ src/app/utilities/apiService.js
@@ -1,16 +1,16 @@
 // apiService.js
 export const fetchAIResponse = async (message) => {
-  console.log('apikey', process.env.NEXT_PUBLIC_OPENAI_API_KEY);
   try {
-    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
       },
       body: JSON.stringify({
-        prompt: message,
-        max_tokens: 150,
+        "model": "gpt-4o-mini",
+        "messages": [{"role": "user", "content": message}],
+        "temperature": 0.7
       }),
     });
     if (!response.ok) {
@@ -18,8 +18,7 @@ export const fetchAIResponse = async (message) => {
     }
     const data = await response.json();
     // Adjust this based on the API response structure
-    console.log('data', data);
-    return data.choices[0].text.trim();
+    return data.choices[0].message.content;
   } catch (error) {
     console.error(error);
     throw error;
